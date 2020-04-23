2883a88edf0f44e0064270866099b85a0dfddc0f
nickariley <nickariley@gmail.com>
2020-04-23 11:54:53 -0500

    added ToDoItems console app

---
+++ C221Projects/ToDoItems/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool playAgain = true;
+            List<ToDoItem> ToDoList = new List<ToDoItem>();
+            ToDoItem newToDoItem;
+            do
+            {
+                
+                Console.Clear();
+                Console.WriteLine("Please enter a ToDo item description, the due date, and priority of Low, Medium, or High.\n");
+
+                Console.Write("Enter Description:");
+                string description = Console.ReadLine().ToUpper();
+                
+                Console.Write("Enter Due Date:");
+                string dueDate = Console.ReadLine().ToUpper();
+
+                Console.Write("Enter Priority Level - High, Medium or Low:");
+                string priority = Console.ReadLine().ToUpper();
+
+                newToDoItem = new ToDoItem(description, dueDate, priority);
+
+                ToDoList.Add(newToDoItem);
+
+                
+                Console.WriteLine("Would you like to enter another to do item? y/n");
+                string userAnswer = Console.ReadLine().ToUpper();
+
+                if (userAnswer.Contains("Y"))
+                {
+                    continue;
+                }
+
+                else if (userAnswer.Contains("N"))
+                {
+                    break;
+                }
+            }
+            while (playAgain == true);
+
+            //IEnumerable<ToDoItem> listSortedHigh = ToDoList.OrderBy(o => o.Priority == "low").ThenBy(o => o.Priority == "medium").ThenBy(o => o.Priority == "high");
+
+            var highPriorityList = newToDoItem.GetHighPriorityList(ToDoList);
+
+            Console.Clear();
+            
+            foreach (ToDoItem item in highPriorityList)
+            {
+                Console.WriteLine($"\t{item.Description}\t{item.DueDate}\t{item.Priority}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ C221Projects/ToDoItems/ToDoItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ToDoItems
+{
+    class ToDoItem
+    {
+        public string Description { get; set; }
+
+        public string DueDate { get; set; }
+
+        public string Priority { get; set; }
+
+        public ToDoItem(string description, string dueDate, string priority)
+        {
+            Description = description;
+            DueDate = dueDate;
+            Priority = priority;
+        }
+
+        public IEnumerable<ToDoItem> GetHighPriorityList(List<ToDoItem> ToDoList)
+        {
+            IEnumerable < ToDoItem > listSortedHigh = ToDoList.OrderBy(o => o.Priority == "LOW").ThenBy(o => o.Priority == "MEDIUM").ThenBy(o => o.Priority == "HIGH");
+
+            return listSortedHigh;
+
+            
+        }
+
+
+    }
+}
